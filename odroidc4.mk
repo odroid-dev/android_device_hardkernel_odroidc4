1ed2cad950b92c93ea3df1a92dce45548d691c21
Chris KIM <codewalker@hardkernel.com>
2020-01-07 17:49:19 +0900

    ODROID-C4: Update BSP 20191101
    Change-Id: I1471314ad1b2508f52dbb35cfebf5cdabb2f4c89

--- odroidc4.mk
+++ odroidc4.mk
@@ -89,14 +89,15 @@ TARGET_KERNEL_BUILT_FROM_SOURCE := true
 
 PRODUCT_TYPE := mbox
 
-BOARD_AML_VENDOR_PATH := vendor/amlogic/common/
-BOARD_WIDEVINE_TA_PATH := vendor/amlogic/
-
+BOARD_AML_TDK_KEY_PATH := device/hardkernel/common/tdk_keys/
 WITH_LIBPLAYER_MODULE := false
 
 OTA_UP_PART_NUM_CHANGED := false
 
-BOARD_AML_TDK_KEY_PATH := device/amlogic/common/tdk_keys/
+BOARD_AML_VENDOR_PATH := vendor/amlogic/common/
+
+BOARD_WIDEVINE_TA_PATH := vendor/amlogic/
+
 #AB_OTA_UPDATER :=true
 BUILD_WITH_AVB := false
 
@@ -218,13 +219,8 @@ PRODUCT_COPY_FILES += \
 #                                                WiFi
 #
 #########################################################################
-#USE_AML_WIFI := true
 WIFI_MODULE := multiwifi
-ifeq ($(USE_AML_WIFI), true)
-include hardware/amlogic/wifi/configs/wifi.mk
-else
 include device/hardkernel/common/wifi.mk
-endif
 
 # Change this to match target country
 # 11 North America; 14 Japan; 13 rest of world
@@ -315,7 +311,13 @@ $(call inherit-product, device/hardkernel/common/media.mk)
 #
 #########################################################################
 
-$(call inherit-product, build/target/product/languages_full.mk)
+# For all locales, $(call inherit-product, build/target/product/languages_full.mk)
+PRODUCT_LOCALES := en_US en_AU en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ pl_PL ja_JP \
+  zh_TW zh_CN zh_HK ru_RU ko_KR nb_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG \
+  ca_ES en_GB fi_FI hi_IN hr_HR hu_HU in_ID iw_IL lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA \
+  vi_VN tl_PH ar_EG fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN en_XA ar_XB fr_CA km_KH \
+  lo_LA ne_NP si_LK mn_MN hy_AM az_AZ ka_GE my_MM mr_IN ml_IN is_IS mk_MK ky_KG eu_ES gl_ES \
+  bn_BD ta_IN kn_IN te_IN uz_UZ ur_PK kk_KZ
 
 #################################################################################
 #
