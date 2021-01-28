018e0025077530e52c87245e5e801369474335bf
Luke go <sangch.go@gmail.com>
2021-01-28 17:18:25 +0900

    ODROID-C4: media: Add new media_profiles.xml files for hevc and h264.
    - The media_profiles_V1_0.xml file is used to select the encoder for
      camera profiles. To use both hevc and h264 encoder, Separate this file
      for each others.
    - It can be selected by media.settings.xml property from default.prop.
    
    Change-Id: I7ed59f6af7ed294119f03cef71dfb7100d2699a2

--- device.mk
+++ device.mk
@@ -39,7 +39,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     device/hardkernel/odroidc4/files/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
-    device/hardkernel/odroidc4/files/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
+    device/hardkernel/odroidc4/files/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_profiles_V1_0.xml \
+    device/hardkernel/odroidc4/files/media_profiles_V1_0_hevc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0_hevc.xml \
+    device/hardkernel/odroidc4/files/media_profiles_V1_0_h264.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0_h264.xml \
     device/hardkernel/odroidc4/files/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     device/hardkernel/odroidc4/files/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     device/hardkernel/odroidc4/files/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
